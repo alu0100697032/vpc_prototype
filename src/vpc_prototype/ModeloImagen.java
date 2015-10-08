1dfd5ff7587630952775810e5bbba960376d903f
Victor Hernandez <alu0100697032@ull.edu.es>
2015-10-08 11:17:02 +0100

    RGB to grey

--- src/vpc_prototype/ModeloImagen.java
+++ src/vpc_prototype/ModeloImagen.java
@@ -3,6 +3,7 @@ package vpc_prototype;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -13,7 +14,9 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class ModeloImagen extends Observable {
 
 	private BufferedImage imagen;
-	private ArrayList<Integer[][]> coloresRGBImagen;
+	private ArrayList<int[][]> coloresRGBImagen;
+	private int[][] imagenGris;
+	private HashMap<Integer, Integer> histograma;
 
 	public ModeloImagen() {
 
@@ -36,24 +39,55 @@ public class ModeloImagen extends Observable {
 		}
 		setImagen(img);
 		almacenarRGBImagen();
+		histograma = new HashMap<Integer, Integer>();
+		for(int i = 0; i < 256; i++)
+			histograma.put(i, 0);
 	}
+
 	/*
-	 * Separar el RGB de la imagen en matrices 
+	 * Separar el RGB de la imagen en matrices
 	 */
 	public void almacenarRGBImagen() {
-		coloresRGBImagen = new ArrayList<Integer[][]>();
-		int[][] matrizAuxiliarPlanoColorAzul = null; 
+		coloresRGBImagen = new ArrayList<int[][]>();
+		int[][] matrizAuxiliarPlanoColorAzul = null;
 		int[][] matrizAuxiliarPlanoColorVerde = null;
-		int[][] matrizAuxiliarPlanoColorRojo = null; 
-		
-		for(int i = 0; i < imagen.getHeight(); i++){
+		int[][] matrizAuxiliarPlanoColorRojo = null;
+
+		for (int i = 0; i < imagen.getHeight(); i++) {
 			matrizAuxiliarPlanoColorAzul[i] = new int[imagen.getWidth()];
 			matrizAuxiliarPlanoColorVerde[i] = new int[imagen.getWidth()];
 			matrizAuxiliarPlanoColorRojo[i] = new int[imagen.getWidth()];
-			for(int j = 0; j < imagen.getWidth(); j++){
-				matrizAuxiliarPlanoColorAzul[i][j] = imagen.getColorModel().getBlue(imagen.getRGB(i, j));
-				matrizAuxiliarPlanoColorVerde[i][j] = imagen.getColorModel().getGreen(imagen.getRGB(i, j));
-				matrizAuxiliarPlanoColorRojo[i][j] = imagen.getColorModel().getRed(imagen.getRGB(i, j));
+			for (int j = 0; j < imagen.getWidth(); j++) {
+				matrizAuxiliarPlanoColorAzul[i][j] = imagen.getColorModel()
+						.getBlue(imagen.getRGB(i, j));
+				matrizAuxiliarPlanoColorVerde[i][j] = imagen.getColorModel()
+						.getGreen(imagen.getRGB(i, j));
+				matrizAuxiliarPlanoColorRojo[i][j] = imagen.getColorModel()
+						.getRed(imagen.getRGB(i, j));
+			}
+		}
+		coloresRGBImagen.add(matrizAuxiliarPlanoColorRojo);
+		coloresRGBImagen.add(matrizAuxiliarPlanoColorVerde);
+		coloresRGBImagen.add(matrizAuxiliarPlanoColorAzul);
+	}
+
+	/*
+	 * Pasar a gris la imagen
+	 */
+	public void pasarImagenGris() {
+		
+		imagenGris = null;
+		for (int i = 0; i < imagen.getHeight(); i++) {
+			imagenGris[i] = new int[imagen.getWidth()];
+		}
+
+		for (int i = 0; i < imagenGris.length; i++) {
+			for (int j = 0; j < imagenGris[i].length; j++) {
+				//NTSC format
+				imagenGris[i][j] = (int) ((coloresRGBImagen.get(0)[i][j] * 0.299)
+						+ (coloresRGBImagen.get(1)[i][j] * 0.587) + (coloresRGBImagen
+						.get(2)[i][j] * 0.114));
+				histograma.put(imagenGris[i][j], histograma.get(imagenGris[i][j]+1));
 			}
 		}
 	}
@@ -70,11 +104,11 @@ public class ModeloImagen extends Observable {
 		this.imagen = imagen;
 	}
 
-	public ArrayList<Integer[][]> getColoresRGB() {
+	public ArrayList<int[][]> getColoresRGB() {
 		return coloresRGBImagen;
 	}
 
-	public void setColoresRGB(ArrayList<Integer[][]> coloresRGB) {
+	public void setColoresRGB(ArrayList<int[][]> coloresRGB) {
 		this.coloresRGBImagen = coloresRGB;
 	}
 
